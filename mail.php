645cd6f87a9d54f9980672586f5e6b2c23c30a07
adarsh-this-side <84468262+adarsh-this-side@users.noreply.github.com>
2021-06-20 13:25:02 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 $name = $_POST['inputName'];
 $email= $_POST['inputEmail'];
 $phone= $_POST['inputPhone'];
-$message= $_POST['message'];
+$message= $_POST['inputMessage'];
 $to = "adarsh.this.side@gmail.com";
 $subject = "Mail From trendbrains";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Phone = " . $phone . "\r\n Message =" . $message;

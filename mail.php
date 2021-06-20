95e4495c947429ae4a7081c088f9e764e27c64c7
adarsh-this-side <84468262+adarsh-this-side@users.noreply.github.com>
2021-06-20 13:16:03 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+
+$name = $_POST['inputName'];
+$email= $_POST['inputEmail'];
+$phone= $_POST['inputPhone'];
+$message= $_POST['message'];
+$to = "adarsh.this.side@gmail.com";
+$subject = "Mail From trendbrains";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Phone = " . $phone . "\r\n Message =" . $message;
+$headers = "From: noreply@hapvey.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+//header("Location:thankyou.html");
+?>

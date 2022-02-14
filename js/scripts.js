70007120821e6f9515af298a33aa0e133ccd0622
adarsh-this-side <84468262+adarsh-this-side@users.noreply.github.com>
2022-02-14 21:38:20 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,15 +33,3 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-
-<script type="text/javascript">
-    var cId =  "162514";
-
-    (function(d, t) {
-      var s = document.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/")  + "cuelinksv2.js";
-      document.getElementsByTagName("body")[0].appendChild(s);
-    }());
-</script>
